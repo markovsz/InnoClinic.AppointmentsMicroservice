dd93595a5c88c5b096eef8adf2b766e89248d586
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-25 03:17:18 +0300

    add entities

---
+++ InnoClinic.AppointmentsMicroservice/Domain/Entities/Result.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+public class Result : BaseEntity
+{
+    public string Complaints { get; set; }
+    public string Conclusion { get; set; }
+    public string Recomendations { get; set; }
+    public DateTime DateTime { get; set; }
+    public Guid AppointmentId { get; set; }
+    public Appointment Appointment { get; set; }
+}
