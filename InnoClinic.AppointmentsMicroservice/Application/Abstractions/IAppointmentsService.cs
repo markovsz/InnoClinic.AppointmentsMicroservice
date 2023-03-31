2792afc0069fd1254b781288b7828b82003aa452
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-31 19:03:01 +0300

    add service abstractions

---
+++ InnoClinic.AppointmentsMicroservice/Application/Abstractions/IAppointmentsService.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs.Incoming;
+using Application.DTOs.Outgoing;
+using Domain.RequestParameters;
+
+namespace Application.Abstractions;
+
+public interface IAppointmentsService
+{
+    Task<Guid> CreateAsync(AppointmentIncomingDto incomngDto);
+    Task ApproveAsync(Guid id);
+    Task<IEnumerable<PatientAppointmentsOutgoingDto>> GetAsync(Guid patientId);
+    Task<IEnumerable<AppointmentByReceptionistOutgoingDto>> GetByReceptionistAsync(AppointmentParameters parameters);
+    Task<IEnumerable<AppointmentScheduleByDoctorOutgoingDto>> GetScheduleByDoctorAsync(ScheduleParameters parameters);
+    Task RescheduleAsync(Guid id, RescheduleAppointmentIncomingDto incomingDto);
+    Task DeleteByIdAsync(Guid id);
+}
---
+++ InnoClinic.AppointmentsMicroservice/Application/DTOs/Incoming/ResultIncomingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.Incoming;
+
+public class ResultIncomingDto
+{
+    public Guid AppointmentId { get; set; }
+    public string Complaints { get; set; }
+    public string Conclusion { get; set; }
+    public string Recomendations { get; set; }
+}
