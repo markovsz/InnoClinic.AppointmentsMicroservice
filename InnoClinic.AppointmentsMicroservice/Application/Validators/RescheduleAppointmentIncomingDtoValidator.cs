03ddc54d709c2d4ecafaa77edda349eaa9496d21
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-01 02:23:08 +0300

    add validators

---
+++ InnoClinic.AppointmentsMicroservice/Application/Validators/RescheduleAppointmentIncomingDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.Incoming;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class RescheduleAppointmentIncomingDtoValidator : AbstractValidator<RescheduleAppointmentIncomingDto>
+{
+	public RescheduleAppointmentIncomingDtoValidator()
+	{
+		RuleFor(e => e.DoctorFirstName).MinimumLength(1);
+		RuleFor(e => e.DoctorLastName).MinimumLength(1);
+		RuleFor(e => e.DoctorMiddleName).MinimumLength(1);
+
+	}
+}
---
+++ InnoClinic.AppointmentsMicroservice/Application/Validators/ScheduleParametersValidator.cs
@@ -0,0 +1,14 @@
+﻿using Domain.RequestParameters;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class ScheduleParametersValidator : AbstractValidator<ScheduleParameters>
+{
+	public ScheduleParametersValidator()
+	{
+		RuleFor(e => e.Day).GreaterThanOrEqualTo(1).LessThanOrEqualTo(31);
+		RuleFor(e => e.Month).GreaterThanOrEqualTo(1).LessThanOrEqualTo(12);
+		RuleFor(e => e.Year).GreaterThanOrEqualTo(1).LessThanOrEqualTo(9999);
+	}
+}
