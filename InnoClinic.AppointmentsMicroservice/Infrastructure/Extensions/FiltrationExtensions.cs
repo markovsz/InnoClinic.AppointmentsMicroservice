d8d54a4218887ea545a9b64326c78ab80876a101
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-01 02:24:54 +0300

    add filter extensions

---
+++ InnoClinic.AppointmentsMicroservice/Infrastructure/Extensions/FiltrationExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Entities;
+using Domain.RequestParameters;
+
+namespace Infrastructure.Extensions;
+
+public static class FiltrationExtensions
+{
+    public static IQueryable<Appointment> AppointmentsFilter(this IQueryable<Appointment> entities, AppointmentParameters parameters)
+    {
+        if(parameters.Date.HasValue)
+            entities = entities.Where(e => e.Date.Equals(parameters.Date));
+
+        if(parameters.DoctorFirstName is not null)
+            entities = entities.Where(e => e.DoctorFirstName.Contains(parameters.DoctorFirstName));
+
+        if(parameters.DoctorLastName is not null)
+            entities = entities.Where(e => e.DoctorLastName.Contains(parameters.DoctorLastName));
+
+        if(parameters.DoctorMiddleName is not null)
+            entities = entities.Where(e => e.DoctorMiddleName.Contains(parameters.DoctorMiddleName));
+
+        if(parameters.ServiceName is not null)
+            entities = entities.Where(e => e.ServiceName.Contains(parameters.ServiceName));
+        
+        if(parameters.IsApproved.HasValue)
+            entities = entities.Where(e => e.IsApproved.Equals(parameters.IsApproved));
+
+        if(parameters.OfficeId.HasValue)
+            entities = entities.Where(e => e.OfficeId.Equals(parameters.OfficeId));
+
+        if (parameters.IsApproved.HasValue)
+            entities = entities.Where(e => e.IsApproved.Equals(parameters.IsApproved));
+        return entities;
+    }
+
+    public static IQueryable<Appointment> ScheduleFilter(this IQueryable<Appointment> entities, ScheduleParameters parameters)
+    {
+        entities = entities.Where(e => e.PatientId.Equals(parameters.PatientId));
+
+        if (parameters.Day.HasValue)
+            entities = entities.Where(e => e.Date.Day.Equals(parameters.Day));
+
+        if (parameters.Month.HasValue)
+            entities = entities.Where(e => e.Date.Month.Equals(parameters.Month));
+
+        if (parameters.Year.HasValue)
+            entities = entities.Where(e => e.Date.Year.Equals(parameters.Year));
+        return entities;
+    }
+}
