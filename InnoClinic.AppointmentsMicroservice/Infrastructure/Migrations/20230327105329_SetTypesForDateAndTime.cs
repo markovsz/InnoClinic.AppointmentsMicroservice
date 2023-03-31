2f4b577782a2f065462676cb2090be4666ecfdeb
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-31 19:08:01 +0300

    add migrations

---
+++ InnoClinic.AppointmentsMicroservice/Infrastructure/Migrations/20230327105329_SetTypesForDateAndTime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetTypesForDateAndTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Time",
+                table: "Appointments",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Appointments",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Time",
+                table: "Appointments",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Appointments",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
